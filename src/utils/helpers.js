90425603a86ab85c6f0f9f02dd75ba7841d7b313
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-01-14 23:30:25 +0000

    New helpers and moved ee from index, header used properly

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,20 +1,15 @@
 const EventEmitter = require('events');
 const ee = new EventEmitter();
 ee.on('log', (...args) => console.log(`${new Date().toLocaleDateString('pt-br')} ${new Date().toLocaleTimeString('pt-br')}`, args));
-const addHeaders = res => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, HEAD, OPTIONS, TRACE, PATCH');
-	res.setHeader('Content-Type', 'application/json');
-}
+const addHeaders = (res, statusCode) => res.writeHead(statusCode, { 'Content-Type': 'application/json' });
 module.exports.error = (res, error = 'Unknown error occurred', statusCode = 500) => {
-	addHeaders(res);
-	res.statusCode = statusCode;
-	res.end(JSON.stringify({ status: 'fail', error }, null, 3))
+	ee.emit('log', 'Entered error helper', error);
+	addHeaders(res, statusCode);
+	res.end(JSON.stringify({ status: 'fail', error }, null, 3));
 }
 module.exports.success = (res, data = null) => {
-	addHeaders(res);
-	res.statusCode = 200;
+	addHeaders(res, 200);
 	res.end(JSON.stringify({ status: 'success', data }, null, 3));
 }
 module.exports.log = (...args) => ee.emit('log', args);
+module.exports.nextSerial = (collection = []) => (Math.max.apply(Math, collection.map(item => item._id)) || 0) + 1;
